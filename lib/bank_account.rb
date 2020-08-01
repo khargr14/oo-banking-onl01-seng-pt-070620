cfc595aa781b62cc446ae572e302c9eb511fa605
Keesha Hargrove <kharg14@gmail.com>
2020-08-01 23:19:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,27 +1,38 @@
 class BankAccount
- def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
-  end
+ attr_reader :name
+    attr_accessor :balance, :status
+    @@all = []
 
-  def name
-    @name
-  end
+    def initialize(name, balance=1000, status="open")
+        @name = name
+        @balance = balance
+        @status = status
+        BankAccount.all << self
+    end
 
-  def deposit(amount)
-    @balance += amount
-  end
 
-  def display_balance
-    "Your balance is $#{self.balance}."
-  end
+    def deposit(amount)
+        self.balance += amount
+    end
 
-  def valid?
-    @status == "open" && @balance > 0
-  end
+    def display_balance 
+        "Your balance is $#{self.balance}."
+    end
 
-  def close_account
-    @status = "closed"
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
+    
   end
-end
